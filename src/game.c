2bd81cbc38d483e55c3c7865059e98377052367e
Luke I. Wilson <asmoaesl@gmail.com>
2020-03-20 23:42:39 -0500

    Some source changes

--- src/game.c
+++ src/game.c
@@ -1,5 +1,14 @@
 #include "game.h"
 
+void narrate(char* const text) {
+    printf("*%s*\n", text);
+    WAIT(SEC_TO_READ(text) + 1.0);
+}
+void dialog(char* const name, char* const text) {
+    printf("%s: %s\n", name, text);
+    WAIT(SEC_TO_READ(text) + 1.0);
+}
+
 unsigned words_in_str(char* const text) {
     unsigned words = 1;
     for (char* c = text; *c != '\0'; ++c)
@@ -8,12 +17,11 @@ unsigned words_in_str(char* const text) {
 }
 
 /* Pass an allocated game_settings */
-int string_to_game_settings(char* const input, game_settings* gs) {
+int string_to_game_settings(game_settings* gs, char* const input) {
     gs->wpm = 300;
     return 0;
 }
 
-char* game_settings_to_string(game_settings* const gs) {
-    char* outp = "wpm: ";
-    return strcat(outp, itoa(gs->wpm));
+int game_settings_to_string(char* str, game_settings* const gs) {
+    return sprintf(str, "wpm: %d\n", gs->wpm);
 }
--- src/game.h
+++ src/game.h
@@ -19,17 +19,15 @@ unsigned words_in_str(char* text);
 
 #define WPS (WPM / 60)
 #define SEC_TO_READ(text) (words_in_str(text) / WPS)
-#define NARRATE(text) printf("*%s*\n", text); \
-    WAIT(SEC_TO_READ(text) + 1.0)
-#define DIALOG(name, text) printf("%s: %s\n", name, text); \
-    WAIT(SEC_TO_READ(text) + 1.0)
+void narrate(char* const text);
+void dialog(char* const name, char* const text);
 
 typedef struct game_settings game_settings;
 struct game_settings {
-    int wpm
+    int wpm;
 };
-int string_to_game_settings(char* const input, game_settings* gs); /* errors: 0 = no error, 1 = an error found while parsing */
-char* game_settings_to_string(game_settings* const gs);
+int string_to_game_settings(game_settings* gs, char* const input); /* errors: 0 = no error, 1 = an error found while parsing */
+int game_settings_to_string(char* str, game_settings* const gs);
 
 void level0_begin();
 
